9217120273873df4cc0deb0c1b3b471a5235aa63
imomin <imomin@uoguelph.ca>
2026-02-01 23:58:03 -0500

    Backend unit test applied

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
@@ -8,12 +9,12 @@ module.exports = function (app) {
       changeOrigin: true,
       
       onProxyReq: (proxyReq, _req, _res) => {
-        // eslint-disable-next-line no-console
         console.log('Proxying request to:', 'http://localhost:8080');
         console.log('Original request path:', _req.originalUrl);
         console.log('Proxied request path:', proxyReq.path);
       },
       onError: (err, _req, _res) => {
+        // eslint-disable-next-line no-console
         console.error('Proxy error:', err);
       },
       onProxyRes: (proxyRes, _req, _res) => {
